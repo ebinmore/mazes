2a160f3f374fc3b5bc54e23ac16efeb2a3629e7a
Eric Binmore <eric.binmore@voxmedia.com>
2017-10-02 21:29:47 -0400

    Removes debugging statement

--- binary_tree.rb
+++ binary_tree.rb
@@ -3,7 +3,6 @@ class BinaryTree
   def self.on(grid)
     grid.each_cell do |cell|
       neighbours = []
-      puts "cell[#{cell.row}, #{cell.column}]"
       neighbours << cell.north if cell.north
       neighbours << cell.east if cell.east
 

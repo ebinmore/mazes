420a0f4ecd25c8b53136945cfd273ce5d5d0ec03
Eric Binmore <eric.binmore@voxmedia.com>
2017-10-15 11:05:28 -0400

    Changes bias from east-west to north-south

--- sidewinder.rb
+++ sidewinder.rb
@@ -1,6 +1,7 @@
 class Sidewinder
 
   def self.on(grid)
+
     grid.each_row do |row|
       run = []
 
@@ -11,14 +12,17 @@ class Sidewinder
         at_northern_boundary = (cell.north == nil)
 
         should_close_out =
-          at_eastern_boundary || (!at_northern_boundary && rand(2) == 0)
+          at_northern_boundary || (!at_eastern_boundary && rand(2) == 0)
+          # at_eastern_boundary || (!at_northern_boundary && rand(2) == 0)
 
         if should_close_out
           member = run.sample
-          member.link(member.north) if member.north
+          # member.link(member.north) if member.north
+          member.link(member.east) if member.east
           run.clear
         else
-          cell.link(cell.east)
+          # cell.link(cell.east)
+          cell.link(cell.north)
         end
       end
     end
